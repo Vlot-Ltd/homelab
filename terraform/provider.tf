17448d11df5cadd856d5b52e2c8fa272f204a905
Timo Vlot <timo@vlot.org.uk>
2023-09-06 10:35:51 +0100

    Started redo of terraform

--- terraform/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "proxmox" {
-  pm_api_url      = var.proxmox_api_url
-  pm_user         = var.proxmox_user
-  pm_token        = var.proxmox_token
-  pm_tls_insecure = var.proxmox_ignore_tls
-  pm_parallel     = 2
-}
--- terraform/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.4.6"
- # required_providers {
-    #proxmox = {
-      #version = "~> 2.9.14"
-      #source  = "Telmate/proxmox"
-    #}
-  #}
-}
